b429c8a3b2f73ef65c13980880b4378d000e59e2
1shiharat <akeome1369@gmail.com>
2017-02-20 10:41:57 +0900

    テスト用モジュールを切り分け

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -3,12 +3,12 @@ import $ from "./app/jquery-shim.js"
 import Utils from "./app/utils.js"
 import Accordion from "./app/accordion.js"
 import Anchor from "./app/anchor.js"
-import FixedHeader from "./app/fixedheader.js"
+// import FixedHeader from "./app/fixedheader.js"
 import HeightLine from "./app/heightline.js"
-import ResponsiveTable from "./app/responsive-table.js"
-import Slidebar from "./app/slidebar.js"
-import Tab from "./app/tab.js"
-import Slider from "./app/slider.js"
+// import ResponsiveTable from "./app/responsive-table.js"
+// import Slidebar from "./app/slidebar.js"
+// import Tab from "./app/tab.js"
+// import Slider from "./app/slider.js"
 
 
 class App {
@@ -16,12 +16,12 @@ class App {
         this.Utils = new Utils();
         this.Accordion = new Accordion();
         this.Anchor = new Anchor();
-        this.FixedHeader = new FixedHeader();
+        // this.FixedHeader = new FixedHeader();
         this.HeightLine = new HeightLine();
-        this.ResponsiveTable = new ResponsiveTable();
-        this.Slidebar = new Slidebar();
-        this.Tab = new Tab();
-        this.Slider = new Slider();
+        // this.ResponsiveTable = new ResponsiveTable();
+        // this.Slidebar = new Slidebar();
+        // this.Tab = new Tab();
+        // this.Slider = new Slider();
     }
 }
 
