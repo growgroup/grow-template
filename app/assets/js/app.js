eff0383b6accd8212f7957988b88f6bc58a9d768
ishihara takashi <akeome1369@gmail.com>
2017-05-05 22:23:27 +0900

    Update app.js

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -8,7 +8,7 @@ import HeightLine from "./app/heightline.js"
 import ResponsiveTable from "./app/responsive-table.js"
 import Slidebar from "./app/slidebar.js"
 import Tab from "./app/tab.js"
-// import Slider from "./app/slider.js"
+import CurrentNav from "./app/current-nav.js"
 
 
 class App {
@@ -20,6 +20,7 @@ class App {
         this.HeightLine = new HeightLine();
         this.ResponsiveTable = new ResponsiveTable();
         this.Slidebar = new Slidebar();
+        this.CurrentNav = new CurrentNav();
         this.Tab = new Tab();
         // this.Slider = new Slider();
     }
