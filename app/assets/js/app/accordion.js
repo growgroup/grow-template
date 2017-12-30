36ef45ea6de4f07b7777677626319180e3f2941e
1shiharat <akeome1369@gmail.com>
2017-12-31 00:02:32 +0900

    1ページに対して複数タブが動作するように修正

--- app/assets/js/app/accordion.js
+++ app/assets/js/app/accordion.js
@@ -18,73 +18,73 @@
  *
  */
 
-import  $ from "./jquery-shim.js"
+import $ from './jquery-shim.js';
 
 var defaultOptions = {
-    selector: ".js-accordion",
-    titleTargetAttr: "data-accordion-title",
-    contentTargetAttr: "data-accordion-content",
-    speed: 300,
-    defaultOpen: false
-}
+  selector: '.js-accordion',
+  titleTargetAttr: 'data-accordion-title',
+  contentTargetAttr: 'data-accordion-content',
+  speed: 300,
+  defaultOpen: false
+};
 
 export default class Accordion {
-    /**
-     * 初期化
-     * @param options
-     */
-    constructor(options) {
-        this.options = $.extend(defaultOptions, options);
-        this.init();
+  /**
+   * 初期化
+   * @param options
+   */
+  constructor(options) {
+    this.options = $.extend(defaultOptions, options);
+    this.init();
+  }
+
+  /**
+   * 初期化
+   */
+  init() {
+
+    // ターゲットを取得する
+    this.targetAll = $(this.options.selector);
+
+    // ターゲットが存在しない場合は実行しない
+    if (!this.targetAll.length) {
+      return false;
     }
+    // 実行する
+    this.run();
+  }
 
-    /**
-     * 初期化
-     */
-    init() {
-
-        // ターゲットを取得する
-        this.targetAll = $(this.options.selector);
-
-        // ターゲットが存在しない場合は実行しない
-        if (!this.targetAll.length) {
-            return false;
-        }
-        // 実行する
-        this.run();
+  /**
+   * 実行する
+   */
+  run() {
+    for (var i = 0; i < this.targetAll.length; i++) {
+      var target = $(this.targetAll[i]);
+      target.title = target.find('*[' + this.options.titleTargetAttr + ']');
+      target.content = target.find('*[' + this.options.contentTargetAttr + ']');
+      this.accordion(target);
+      if (this.options.defaultOpen) {
+        target.content.slideDown();
+      }
     }
+  }
 
-    /**
-     * 実行する
-     */
-    run() {
-        for (var i = 0; i < this.targetAll.length; i++) {
-            var target = $(this.targetAll[i]);
-            target.title = target.find('*[' + this.options.titleTargetAttr + ']');
-            target.content = target.find('*[' + this.options.contentTargetAttr + ']');
-            this.accordion(target);
-            if (this.options.defaultOpen) {
-                target.content.slideDown();
-            }
-        }
-    }
-
-    /**
-     * アコーディオンの動作
-     * @param el
-     */
-    accordion(el) {
+  /**
+   * アコーディオンの動作
+   * @param el
+   */
+  accordion(el) {
 
 
-        $(el.title).on('click', (e) => {
-            e.preventDefault();
-            el.content.slideToggle(this.options.speed);
-            el.content.closest(this.options.selector).toggleClass("is-open");
-        });
+    $(el.title).on('click', (e) => {
+      e.preventDefault();
+      el.content.slideToggle(this.options.speed);
+      el.content.closest(this.options.selector).toggleClass('is-open');
+    });
 
-        $(el.title).on('mouseover', (e) => {
-            $(this).css('cursor', 'pointer');
-        });
+    $(el.title).on('mouseover', (e) => {
+      $(this).css('cursor', 'pointer');
+    });
 
-    }
+  }
 }
--- app/assets/js/app/anchor.js
+++ app/assets/js/app/anchor.js
@@ -13,78 +13,76 @@
  */
 'use strict';
 
-import  $ from "./jquery-shim.js"
+import $ from './jquery-shim.js';
 
 /**
  * デフォルトオプション
  * @type {{}}
  */
 var defaultOptions = {
-    selector: '.js-anchor',
-    dataSelector: "anchor-target",
-    scrollSpeed: 300,
-    easing: "linear",
-}
-
+  selector: '.js-anchor',
+  dataSelector: 'anchor-target',
+  scrollSpeed: 300,
+  easing: 'linear'
+};
 export default class Anchor {
-
-    constructor(options) {
-        // オプションをセット
-        this.options = $.extend(defaultOptions, options);
-        this.init();
-    }
-
-
-    /**
-     * 初期化
-     * @param e ターゲットとなるエレメント
-     */
-    init() {
-        this.target = $(this.options.selector);
-        this.onClick();
-    }
-
-    /**
-     * クリック時のイベント
-     */
-    onClick() {
-
-        var self = this;
-
-        this.target.on('click', function (e) {
-            e.preventDefault();
-
-            // スクロール先のターゲットを指定
-            var anchorTargetSelector = $(this).data(self.options.dataSelector);
+  constructor(options) {
+    // オプションをセット
+    this.options = $.extend(defaultOptions, options);
+    this.init();
+  }
 
 
-            if (typeof anchorTargetSelector === "undefined") {
-                var href = $(this).attr("href");
-                var anchorTargetSelector = href.match(/#(\S*)/g);
-                if (typeof anchorTargetSelector[0] === "undefined") {
-                    throw new Error("ターゲットとなる要素を取得できませんでした。");
-                    return false;
-                }
-                anchorTargetSelector = anchorTargetSelector[0];
-            }
+  /**
+   * 初期化
+   * @param e ターゲットとなるエレメント
+   */
+  init() {
+    this.target = $(this.options.selector);
+    this.onClick();
+  }
 
-            var anchorTarget = $(anchorTargetSelector);
+  /**
+   * クリック時のイベント
+   */
+  onClick() {
 
-            if (anchorTarget.length === 0) {
-                throw new Error("ターゲットとなる要素を取得できませんでした。");
-                return false;
-            }
-            var top = anchorTarget.offset().top;
+    var self = this;
 
-            // スクロールさせる
-            $('body,html').animate({
-                scrollTop: top,
-            }, {
-                duration: self.options.scrollSpeed,
-                easing: self.options.easing
-            })
-        });
-    }
+    this.target.on('click', function(e) {
+      e.preventDefault();
+
+      // スクロール先のターゲットを指定
+      var anchorTargetSelector = $(this).data(self.options.dataSelector);
+
+
+      if (typeof anchorTargetSelector === 'undefined') {
+        var href = $(this).attr('href');
+        var anchorTargetSelector = href.match(/#(\S*)/g);
+        if (typeof anchorTargetSelector[0] === 'undefined') {
+          throw new Error('ターゲットとなる要素を取得できませんでした。');
+          return false;
+        }
+        anchorTargetSelector = anchorTargetSelector[0];
+      }
+
+      var anchorTarget = $(anchorTargetSelector);
+
+      if (anchorTarget.length === 0) {
+        throw new Error('ターゲットとなる要素を取得できませんでした。');
+        return false;
+      }
+      var top = anchorTarget.offset().top;
+
+      // スクロールさせる
+      $('body,html').animate({
+        scrollTop: top
+      }, {
+        duration: self.options.scrollSpeed,
+        easing: self.options.easing
+      });
+    });
+  }
 }
 
 
